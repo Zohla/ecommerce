e7ace2dc7952ef4b159be79c5dbb0dd8d9b58da9
Maren Lilleberre <marenlilleberre@gmail.com>
2021-11-09 13:17:23 +0100

    removed deprecated functions from react router and added the stuff that works with v.6

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
 
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image' style={{
-        backgroundImage: `url(${imageUrl})`
-    }} />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-                <span className = 'subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({title, imageUrl, size, linkUrl}) => {
+    let navigate = useNavigate();
+    let location = useLocation();
+return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`${location.pathname}${linkUrl}`)}
+    >
+    <div className="background-image" style={
+        {
+            backgroundImage: `url(${imageUrl})`,
+        }
+    }></div>
+    <div className="content">
+        <h1 className = "title">{title.toUpperCase()}</h1>
+        <span className = "subtitle">SHOP NOW</span>
     </div>
-)
+</div>
+)};
 
 export default MenuItem;
